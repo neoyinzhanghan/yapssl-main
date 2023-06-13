added dino attention mask and small fixes

yapssl_models/dino_lightning.py
@@ -33,6 +33,7 @@ class DINO(pl.LightningModule):
         
         super().__init__()
 
+        self.ssl_arch = 'dino'
         # this is if you want to use resnet18 backbone
         # resnet = torchvision.models.resnet18()
         # backbone = nn.Sequential(*list(resnet.children())[:-1])
@@ -42,7 +43,7 @@ class DINO(pl.LightningModule):
         # original paper (you might have to reduce the batch size in this case):
         backbone = torch.hub.load('facebookresearch/dino:main', 'dino_vits16', pretrained=False)
         input_dim = backbone.embed_dim
-
+        
         self.student_backbone = backbone
         self.student_head = DINOProjectionHead(
             input_dim, 512, 64, 2048, freeze_last_layer=1

yapssl_models/ssl_eval.py
@@ -33,7 +33,8 @@ class PatchFeatureExtractor(nn.Module):
         if self.ssl_arch == 'mae':
             extraction_model = mae.MAELightning.load_from_checkpoint(chkpt_fpath)
             extraction_model.eval()
-        if self.ssl_arch == 'dino'
+
+        if self.ssl_arch == 'dino':
             extraction_model = dino.DINOLightning.load_from_checkpoint(chkpt_fpath)
             extraction_model.eval()
 